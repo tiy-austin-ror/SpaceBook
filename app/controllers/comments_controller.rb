3f8b710063f7608c438faef01de442c424ccabdb
Vivek Patel <vnp229@gmail.com>
2016-04-21 18:33:34 -0500

    added body col in comments, added create and update for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,46 @@
 class CommentsController < ApplicationController
+  def create
+    #Builds the comment for the current user
+    #params[:id] should be the id of the Event
+    @comment = current_user.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to campus_room_event(params[:campus_id],
+                                      params[:room_id],
+                                      params[:id]) }
+        format.json { render json: @comment }
+      else
+        format.html { redirect_to campus_room_event(params[:campus_id],
+                                      params[:room_id],
+                                      params[:id]) }
+        format.json { render json: @comment.errors }
+      end
+    end
+  end
+
+  def update
+    @comment = Comment.find(get_comment)
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to campus_room_event(params[:campus_id],
+                                      params[:room_id],
+                                      params[:id]) }
+        format.json { render json: @comment }
+      else
+        format.html { redirect_to campus_room_event(params[:campus_id],
+                                      params[:room_id],
+                                      params[:id]) }
+        format.json { render json: @comment.errors }
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :event_id)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+
+  validates :body, presence: true, length: { in: 2..500 }
 end
