096592f1ca83523a412b708f92ccad433d3ba1a0
JaredPowers <jarpow44@gmail.com>
2016-04-21 17:26:55 -0500

    added controller and views for campuses

--- app/controllers/campuses_controller.rb
+++ app/controllers/campuses_controller.rb
@@ -1,2 +1,59 @@
 class CampusesController < ApplicationController
+
+  def index
+    @campuses = Campus.all
+  end
+
+  def show
+    @campus = Campus.find(params[:id])
+  end
+
+  def new
+    @campus = Campus.new
+  end
+
+  def create
+    @campus = Campus.new(campus_params)
+
+    respond_to do |format|
+      if @campus.save
+        format.html { redirect_to @campus, notice: 'New Campus was successfully created.' }
+        format.json { render :show, status: :created, location: @campus }
+      else
+        format.html { render :new }
+        format.json { render json: @campus.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @campus.update(campus_params)
+        format.html { redirect_to @campus, notice: 'Campus was successfully updated.' }
+        format.json { render :show, status: :ok, location: @campus }
+      else
+        format.html { render :edit }
+        format.json { render json: @campus.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @campus.destroy
+    respond_to do |format|
+      format.html { redirect_to campuses_url, notice: 'Campus was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_campus
+      @campus = Campus.find(params[:id])
+    end
+
+    def campus_params
+      params.require(:campus)
+    end
+
 end
