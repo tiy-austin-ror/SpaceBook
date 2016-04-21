7bceb407acbbd8e67c53a6356e0a3a79bedd73d4
Justin Herrick <justin@justinherrick.com>
2016-04-21 16:22:35 -0500

    Adding secrets.yml back to the project.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Spacebook
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.secret_key_base = Rails.env.production? ? ENV['SECRET_TOKEN'] : 'super_secret_token'
   end
 end
