d3da6fbe69a2e23959c7b1886bf836bdd1391127
Vivek Patel <vnp229@gmail.com>
2016-04-21 16:23:16 -0500

    removed coffee files and fixture files, added reference event belongs_to user

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   has_many :invites
   has_many :comments
   belongs_to :room
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_one :campus_user
   has_many :invites
   has_many :comments
+  has_many :events
 end
