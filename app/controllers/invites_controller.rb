b7b162aa3c8898b1f889029b7bd31f981697f3b3
Brent Martin <bmart35@gmail.com>
2016-04-21 23:38:27 -0500

    inserted strong params

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,2 +1,20 @@
 class InvitesController < ApplicationController
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def get_invite
+    Invite.find(params[:invite_id])
+  end
+
+  def invite_params
+    params.require(:invite).permit(:status, :invite_id)
+  end
 end
