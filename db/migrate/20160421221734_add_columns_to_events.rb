aa97d76ae0a8eb55d0d861dd0208d45b48d74335
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-21 19:09:36 -0500

    added view show and edit

--- db/migrate/20160421221734_add_columns_to_events.rb
+++ db/migrate/20160421221734_add_columns_to_events.rb
@@ -3,7 +3,6 @@ class AddColumnsToEvents < ActiveRecord::Migration
     add_column :events, :name, :string
     add_column :events, :start_time, :date
     add_column :events, :duration, :integer
-    add_reference :events, :user, index: true, foreign_key: true
     add_column :events, :agenda, :text
     add_column :events, :description, :text
     add_column :events, :private, :bool
