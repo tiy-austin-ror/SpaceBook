fd38cea98d9959b19ca90ddfbc9ff5d17f273398
Vivek Patel <vnp229@gmail.com>
2016-04-23 15:38:22 -0500

    added react component for open/closed invite

--- app/assets/javascripts/components/private_event.js.jsx
+++ app/assets/javascripts/components/private_event.js.jsx
@@ -1,13 +1,40 @@
 var PrivateEvent = React.createClass({
+  getInitialState: function () {
+    return {
+      private: false
+    };
+  },
+
+  handlePrivateClick: function () {
+    this.setState({
+      private: true
+    });
+  },
+
+  handlePublicClick: function () {
+    this.setState({
+      private: false
+    });
+  },
+
   render: function(){
     return (
       <div>
-        <span className="col-sm-3 col-md-2"> <span className="sm-margin-right"> Public</span>
-          <input type="radio" name="event[private]" value="false" />
-        </span>
-        <span className="col-sm-3 col-md-2 sm-margin-right"> <span className="sm-margin-right">Private</span>
-          <input type="radio" name="event[private]" value="true" defaultChecked/>
-        </span>
+        <div className="row">
+          <span className="col-sm-3 col-md-2"> <span className="sm-margin-right"> Public</span>
+            <input type="radio"
+                   name="event[private]"
+                   value="false" defaultChecked
+                   onClick={this.handlePublicClick}/>
+          </span>
+          <span className="col-sm-3 col-md-2 sm-margin-right"> <span className="sm-margin-right">Private</span>
+            <input type="radio"
+                   name="event[private]"
+                   value="true"
+                   onClick={this.handlePrivateClick}/>
+          </span>
+        </div>
+        <OpenInvite private={this.state.private} />
       </div>
     );
   }
