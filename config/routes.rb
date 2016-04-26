ab0d961ba1bacc4d1c19a8b3d8f0cadad0959097
Jordan Corley <rainwolfj@gmail.com>
2016-04-26 16:29:24 -0500

    Adjusted so creation still runs through the homemade user controller instead of running across the clearance one.

--- config/routes.rb
+++ config/routes.rb
@@ -11,14 +11,6 @@ Rails.application.routes.draw do
   end
 
   constraints Clearance::Constraints::SignedIn.new do
-    resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-    resource :session, controller: "clearance/sessions", only: [:create]
-
-    resources :users, controller: "clearance/users", only: [:create] do
-      resource :password,
-        controller: "clearance/passwords",
-        only: [:create, :edit, :update]
-    end
 
     delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
     resources :users
@@ -35,15 +27,24 @@ Rails.application.routes.draw do
     post 'invite_codes' => "invite_codes#create"
     get 'admin' => "users#admin_dashboard"
     root 'dashboard#home'
+
+    resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+    resource :session, controller: "clearance/sessions", only: [:create]
+
+    resources :users, controller: "clearance/users", only: [:create] do
+      resource :password,
+        controller: "clearance/passwords",
+        only: [:create, :edit, :update]
+    end
   end
 
   constraints Clearance::Constraints::SignedOut.new do
     resources :passwords, controller: "clearance/passwords", only: [:create, :new]
     resource :session, controller: "clearance/sessions", only: [:create]
 
-    resources :users, only: [:create, :new]
+    resources :users, only: [:new, :create]
 
-    resources :users, controller: "clearance/users", only: [:create] do
+    resources :users, controller: "clearance/users", except: [:create] do
       resource :password,
         controller: "clearance/passwords",
         only: [:create, :edit, :update]
@@ -52,7 +53,7 @@ Rails.application.routes.draw do
     get "/sign_in" => "clearance/sessions#new", as: "sign_in"
     get "/sign_up" => "clearance/users#new", as: "sign_up"
 
-    root 'dashboard#home', as: :guest_root
+    root "clearance/sessions#new", as: :guest_root
   end
 
 end
--- features/signing.feature
+++ features/signing.feature
@@ -6,7 +6,7 @@ Feature: Signing In/Out
       And I fill in "Email" with "test_admin@spacebook.com"
       And I fill in "Password" with "password"
       And I press "Sign in"
-      Then I should see "Welcome: Admin User"
+      Then I should see "click an event to edit it:"
 
     Scenario: A User Signs In
       Given I have an existing account
