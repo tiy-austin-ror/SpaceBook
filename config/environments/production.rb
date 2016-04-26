a0cd1ad34a0454982c6370c79c55817529628c39
Brent Martin <bmart35@gmail.com>
2016-04-26 17:22:57 -0500

    switched domain config in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "https://tiy-spacebook.herokuapp.com/",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
