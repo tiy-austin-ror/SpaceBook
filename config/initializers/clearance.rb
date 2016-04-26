74fa2b512e67d979b7ade29bc2fdf059a8135c98
Jordan Corley <rainwolfj@gmail.com>
2016-04-26 14:43:15 -0500

    Functional clearance security on routes, will need to answer questions about admin routing, then remove the Vanderson Protocols where present.

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
+  config.routes = false
   config.mailer_sender = "do_not_reply@spacebook.com"
   config.redirect_url = '/'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,76 @@
 Rails.application.routes.draw do
 
-  resources :users
-  resources :invites
-  resources :comments
-  resources :amenities
+  # resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  # resource :session, controller: "clearance/sessions", only: [:create]
+  #
+  # resources :users, controller: "clearance/users", only: [:create] do
+  #   resource :password,
+  #     controller: "clearance/passwords",
+  #     only: [:create, :edit, :update]
+  # end
+  #
+  # get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  # delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  # get "/sign_up" => "clearance/users#new", as: "sign_up"
+  # resources :users
+  # resources :invites
+  # resources :comments
+  # resources :amenities
+  #
+  # resources :campuses do
+  #   resources :rooms do
+  #     resources :events
+  #   end
+  # end
+  #
+  # post 'invite_codes' => "invite_codes#create"
+  # get 'admin' => "users#admin_dashboard"
+  # root 'dashboard#home'
+  #
+  constraints Clearance::Constraints::SignedIn.new do
+    resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+    resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :campuses do
-    resources :rooms do
-      resources :events
+    resources :users, controller: "clearance/users", only: [:create] do
+      resource :password,
+        controller: "clearance/passwords",
+        only: [:create, :edit, :update]
     end
+
+    delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+    resources :users
+    resources :invites
+    resources :comments
+    resources :amenities
+
+    resources :campuses do
+      resources :rooms do
+        resources :events
+      end
+    end
+
+    post 'invite_codes' => "invite_codes#create"
+    get 'admin' => "users#admin_dashboard"
+    root 'dashboard#home'
   end
 
-  post 'invite_codes' => "invite_codes#create"
-  get 'admin' => "users#admin_dashboard"
-  root 'dashboard#home'
+  constraints Clearance::Constraints::SignedOut.new do
+    resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+    resource :session, controller: "clearance/sessions", only: [:create]
+
+    resources :users, only: [:create, :new]
+
+    resources :users, controller: "clearance/users", only: [:create] do
+      resource :password,
+        controller: "clearance/passwords",
+        only: [:create, :edit, :update]
+    end
+
+    get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+    get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+    root 'dashboard#home', as: "guest_root"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
