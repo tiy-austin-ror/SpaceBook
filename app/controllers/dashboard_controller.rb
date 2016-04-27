da396e58b4e25d6b729e4a3d6752cb06a418e91e
JaredPowers <jarpow44@gmail.com>
2016-04-27 09:17:25 -0500

    buttons function, still needs cuking

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,11 @@
 class DashboardController < ApplicationController
-before_action :require_login
+  before_action :require_login
+
   def home
     @user = current_user
+    @user_pending_invites = @user.invites.where(status: Invite::PENDING).includes(:event)
     @events = @user.upcoming_events.order("start_time")
     @pending = @user.upcoming_meetings.order("start_time")
     @finished = @user.finished_events.order("start_time")
   end
-
-  private
-    def get_user
-      User.find(current_user.id)
-    end
 end
