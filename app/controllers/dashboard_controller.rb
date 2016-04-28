95cccd9d9809981e536a3ca32711ac182f55fc18
Vivek Patel <vnp229@gmail.com>
2016-04-28 14:53:55 -0500

    removed anotehr n+1

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,13 +8,13 @@ class DashboardController < ApplicationController
     @user_pending_invites = @user.invites.where(status: Invite::PENDING).includes(:event, :room)
     @user_accepted_invites = @user.invites.where(status: Invite::ACCEPTED).includes(:event, :room)
     @user_accepted_invites += @user.invites.where(status: Invite::REMOTE).includes(:event, :room)
-    @events = @user.upcoming_events.order("start_time").includes(:room)
-    @finished = @user.finished_events.order("start_time").includes(:room)
+    @events = @user.upcoming_events.order("start_time")
+    @finished = @user.finished_events.order("start_time")
   end
 
   def admin_dashboard
     @user = current_user
-    @user_pending_invites = @user.invites.where(status: Invite::PENDING).includes(:event)
+    @user_pending_invites = @user.invites.where(status: Invite::PENDING).includes(:event, :room)
     @events = @user.upcoming_events.order("start_time")
     @finished = @user.finished_events.order("start_time")
     @company = @user.company
