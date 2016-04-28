6bc2566a399b7546f32342adee9ac783466d7e61
Vivek Patel <vnp229@gmail.com>
2016-04-28 14:52:41 -0500

    added includes room to events and finished on dashbaord controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,7 @@ class DashboardController < ApplicationController
     @user_pending_invites = @user.invites.where(status: Invite::PENDING).includes(:event, :room)
     @user_accepted_invites = @user.invites.where(status: Invite::ACCEPTED).includes(:event, :room)
     @user_accepted_invites += @user.invites.where(status: Invite::REMOTE).includes(:event, :room)
-    @events = @user.upcoming_events.order("start_time")
-    @finished = @user.finished_events.order("start_time")
+    @events = @user.upcoming_events.order("start_time").includes(:room)
+    @finished = @user.finished_events.order("start_time").includes(:room)
   end
 end
