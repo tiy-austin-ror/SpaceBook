5cbcf31bd2ba485eb5385f105f0a996f90755860
Van Anderson <van.m.anderson@gmail.com>
2016-04-28 14:21:40 -0500

    production settings are in place as well

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,16 +81,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailgun.org",
-    port: 587,
-    domain: ENV['domain'],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['gmail_username'],
-    password: ENV['gmail_password'],
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
+    :user_name      => ENV['username'],
+    :password       => ENV['password'],
+    :authentication => :plain,
   }
-
   config.action_mailer.default_url_options = { host: "tiy-spacebook.herokuapp.com" }
 
 
